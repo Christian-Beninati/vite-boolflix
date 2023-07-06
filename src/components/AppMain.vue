c29e319a235a44590fd565d8c81cda4439ab9987
Christian-Beninati <christianbeninati.info@gmail.com>
2023-07-06 11:20:02 +0100

    add-flag

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,18 +1,43 @@
 <script>
 export default {
-    props: ['movies'], // Props "movies" (dallo Store)
+    props: ['movies'], // Props "movies" 
+
+    computed: {
+        flagImages() {
+            return {
+                en: 'src/assets/img/en.png',
+                it: 'src/assets/img/it.png'
+            };
+        }
+    },
+
+    methods: {
+        hasFlagLanguage(language) {
+            return language === 'en' || language === 'it';
+        },
+        flagImagePath(language) {
+            return this.flagImages[language] || '';
+        }
+    }
 };
 </script>
 
 <template>
-    <div>
-        <div v-for="movie in movies" :key="movie.id">
-            <h3>{{ movie.title }}</h3>
-            <p>Titolo Originale: {{ movie.original_title }}</p>
-            <p>Lingua: {{ movie.original_language }}</p>
-            <p>Voto: {{ movie.vote_average }}</p>
-        </div>
-    </div>
+    <main>
+        <ul>
+            <li v-for="movie in movies" :key="movie.id">
+                <h3>{{ movie.title }}</h3>
+                <p>{{ movie.original_title }}</p>
+                <template v-if="hasFlagLanguage(movie.original_language)">
+                    <img :src="flagImagePath(movie.original_language)" :alt="movie.original_language" />
+                </template>
+                <template v-else>
+                    <p>{{ movie.original_language }}</p>
+                </template>
+                <p>Voto: {{ movie.vote_average }}</p>
+            </li>
+        </ul>
+    </main>
 </template>
 
 <style lang="scss" scoped></style>
